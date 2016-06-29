f2db4ef7a69ee7fe05b0001485f849b1b1635c54
RahulBaboota <rahulbaboota08@gmail.com>
2016-06-30 00:00:11 +0530

    Testing for existence of Geolocation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Checking wether browser supports Geolocation
+
+if ( navigator.geolocation )
+{
+	console.log('Geolocation is supported')
+}
+else
+{
+	console.log('Geolocation is not supported for this browser')
+}
