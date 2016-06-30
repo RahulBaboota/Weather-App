4a524590e5f66f1055c617cbbe14177d7cfb176b
RahulBaboota <rahulbaboota08@gmail.com>
2016-07-01 01:38:24 +0530

    Added Weather Condition Extractor Function

--- js/main.js
+++ js/main.js
@@ -17,3 +17,36 @@ var getLocation = function()
 		'jsonp');
 };
 
+
+// Function which gets weather data from openweatherapi 
+
+var getWeather = function()
+{
+	$.ajax(
+	{
+		url:'http://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&appid=a907b62ef4d40465fbfbe6ca63b1423c',
+		dataType: 'json',
+		type: 'GET',
+		xhrFields:
+		{
+			withCredentials: false,
+		},
+
+		// If the request is successful
+
+		success: function(data)
+		{
+			console.log(data);
+		},
+
+		//If the request is unsuccessful
+
+		error: function(data)
+		{
+			console.log('There was a problem with the API request');
+		}
+	});
+};
+
+getWeather()
+
